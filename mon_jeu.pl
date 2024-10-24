27d2842ee06e87f2ffe50be1c770f91207746125
Enzo Goldstein <75305190+AsiimovOnlyOne@users.noreply.github.com>
2024-10-24 16:01:04 +0200

    Update mon_jeu.pl

--- mon_jeu.pl
+++ mon_jeu.pl
@@ -16,18 +16,16 @@
 :- op(1000, fx, prendre).
 :- op(1000, fx, lacher).
 :- op(1000, fx, aller).
+:- op(1000, fx, dire).
+:- op(1000, fx, voir).
 
 
 
 % position du joueur. Ce predicat sera modifie au fur et a mesure de la partie (avec `retract` et `assert`)
-position_courante(feu_de_camp).
-
-% passages entre les different endroits du jeu
-passage(quelque_part, nord, quelque_part).
+position_courante(camp).
 
 % position des objets
-position(truc, quelque_part).
-
+position(combinaison, vaisseau).
 
 % ramasser un objet
 prendre(X) :-
@@ -50,45 +48,55 @@ prendre(X) :-
         fail.
 
 
-% laisser un objet
-lacher(X) :-
-        position(X, en_main),
-        position_courante(P),
-        retract(position(X, en_main)),
-        assert(position(X, P)),
-        write("OK."), nl,
-        !.
-
-lacher(_) :-
-        write("Vous n'avez pas ca en votre possession !"), nl,
-        fail.
-
-
-% quelques raccourcis
-n :- aller(nord).
-s :- aller(sud).
-e :- aller(est).
-o :- aller(ouest).
-
-
 % deplacements
 aller(musee) :-
-        position_courante(feu_de_camp),
-        retract(position_courante(feu_de_camp)),
+        position_courante(camp),
+        retract(position_courante(camp)),
         assert(position_courante(musee)),
+        regarder, !.
+
+aller(etage) :-
+        position_courante(musee),
+        retract(position_courante(musee)),
+        assert(position_courante(etage)),
+        regarder, !.
+
+aller(musee) :-
+        position_courante(etage),
+        retract(position_courante(etage)),
+        assert(position_courante(musee)),
+        regarder, !.
+
+aller(camp) :-
+        position_courante(musee),
+        retract(position_courante(musee)),
+        assert(position_courante(camp)),
+        regarder, !.
+
+aller(camp) :-
+        position_courante(fusee),
+        retract(position_courante(fusee)),
+        assert(position_courante(camp)),
         regarder, !.
 
 aller(fusee) :-
         position(codes, en_main),
-        position_courante(feu_de_camp),
-        retract(position_courante(feu_de_camp)),
+        position_courante(camp),
+        retract(position_courante(camp)),
         assert(position_courante(fusee)),
         regarder, !.
 
 aller(fusee) :-
+        position_courante(camp),
         write("Vous vous dirigez vers la fusee... Vous voyez un ascenseur vous indiquant que vous 
         n'avez pas les codes de lancement necessaire. Vous faites demi-tour."), nl,
         fail.
+
+aller(espace) :-
+        position_courante(fusee),
+        retract(position_courante(fusee)),
+        assert(position_courante(espace)),
+        regarder, !.
 
 aller(_) :-
         write("Vous ne pouvez pas aller par la."),
@@ -150,26 +158,68 @@ voir(statue) :-
         Soudainement, la statue ouvre les yeux et vous regarde brusquement.
         Vous n'entendez qu'un bruit sourd et voyez ses grands yeux violets, 
         tandis que vos souvenirs depuis votre reveil defilent devant vos yeux.
-        Tout d'un coup, ses yeux s'eteignent et le bruit sourd cesse...
-        "), nl.
+        Tout d'un coup, ses yeux s'eteignent et le bruit sourd cesse..."), nl.
 
 voir(statue) :-
         write("Vous voyez une statue mysterieuse des Nomai, une civilisation ancienne et disparue. Vous lisez :
         'Cette statue est une des rares que nous ayons decouvertes.'
-        'On pense que les Nomai l'ont sculptee pour honorer un evenement important de leur histoire.'
-        "), nl.
+        'On pense que les Nomai l'ont sculptee pour honorer un evenement important de leur histoire.'"), nl.
 
 voir(maquettes) :-
         write("Vous voyez un modele reduit du systeme solaire montrant les orbites des differentes planetes.. 
         Vous lisez les informations suivantes :
         'Voici notre systeme solaire, compose de Timber Hearth et de ses voisines. 
-        Chacune des planetes a ses propres mysteres, qui continuent d'intriguer les chercheurs.'
+        Chacune des planetes a ses propres mysteres, qui continuent d'intriguer les chercheurs.'"), nl.
+
+voir(sigles) :-
+        position_courante(musee),
+        write("
+        
+                           @@@@@@@@
+                        @@@        @@@
+                     @@@              @@@
+                   @@                    @@                    @@@@@@@@@
+                 @@                        @@               @@@         @@@
+                @                            @@          @@@               @@@
+               @                               @       @@                     @@
+@             @                                 @    @@                         @@
+ @            @                                  @  @                             @
+  @           @                                  @ @                               @
+   @           @                                 @@                                @
+    @           @@                               @                                 @
+     @                                          @                                 @
+      @                                        @                                @@
+       @                                      @           @                   @@
+        @@                                  @@             @@              @@@
+          @@                              @@                 @@@        @@@
+            @@                          @@                      @@@@@@@@
+              @@@                    @@@
+                 @@@              @@@
+                    @@@        @@@
+                       @@@@@@@@
+        
         "), nl.
+
+voir(sigles) :-
+        position_courante(musee),
+        position(traducteur, en_main),
+        write("Vous pouvez lire:
+        'Cassava : Nous sommes bientôt prêts ! Filix et moi avons achevé la construction et d'après elle,
+        le calibrage de l'appareil ne devrait pas prendre longremps.'
+        'Filix : Fort heureusement, l'absence d'atmosphère sur la Rocaille facilitera le calibrage.
+        Après tout ce temps, je suis impatiente qu'on reprenne enfin nos recherches !'"), nl.
+
+voir(journal) :-
+        position_courante(fusee),
+        write("C'est mon premier jour dans le programme spatial !
+        J'ai vraiment hate de partir explorer toutes les planetes de notre systeme solaire.
+        Je pars avec le traducteur et grace a lui je suis sur que je pourrai en apprendre davantage sur les numai...
+        Je reussirai a comprendre pourquoi ils on disparu, tout le monde sera fier de moi sur Atrebois !"), nl.
 
 
 % dialogue personnages
 parler :-
-    position_courante(feu_de_camp),
+    position_courante(camp),
     write("Si c'est pas notre pilote ! Je vois que tu es de retour de ta derniere nuit a la belle etoile avant ton decollage.
         Alors ca y est, c'est le grand jour ? J'ai l'impression que tu as rejoint le programme spatial pas plus tard qu'hier, 
         et voila soudain que tu t'appretes a partir pour ton premier voyage en solitaire.
@@ -177,12 +227,12 @@ parler :-
         -> (Options : dire allons-y, dire ok). "), nl.
 
 parler :-
-    position_courante(musee),
+    position_courante(etage),
     write("Te voila ! Je viens de terminer les observations prealables. Les conditions meteorologiques locales sont bonnes. 
         C'est l'heure pour notre plus jeune astronaute de prendre son envol !
         Et tu seras notre toute premiere recrue a partir avec un traducteur de nomai portable !
         Je t'avoue que rien que d'y penser, j'en ai la tete qui tourne.
-        On est mieux equipes que jamais pour lever le voile sur les mysteres des Nomaï. 
+        On est mieux equipes que jamais pour lever le voile sur les mysteres des Nomai. 
         Vous avez fait un travail remarquable, toi et Hal ! Voici les codes de lancement pour utiliser l'ascenseur.
         -> (Obtenu : traducteur, codes)"),
         assert(position(traducteur, en_main)),
@@ -190,19 +240,17 @@ parler :-
 
 % interaction personnages
 dire(allons-y) :-
-    position_courante(feu_de_camp),
+    position_courante(camp),
     write("Ta motivation fait plaisir a voir, mais souviens-toi, si tu bousilles la fusee, viens pas m'en demander un autre. 
         Je ne suis pas en alliage d'aluminium leger resistant a la rentree atmospherique, tu sais.
-        Quoi qu'il en soit, tu vas devoir parler a Cornee a l'observatoire pour obtenir les codes de lancement si tu veux pouvoir decoller. 
-        Amene-les-moi une fois que tu auras fait tes adieux ou je ne sais quoi."), nl.
+        Quoi qu'il en soit, tu vas devoir parler a Cornee a l'observatoire pour obtenir les codes de lancement si tu veux pouvoir decoller."), nl.
 
 dire(ok) :-
-    position_courante(feu_de_camp),
+    position_courante(camp),
     write("Ha ha ! Tout est pret de mon côte - on va enfin pouvoir tester le nouveau systeme d'atterrissage 
         hydraulique avec un pilote plutôt que le systeme automatique ! ... En parlant de pilote, 
         evite de t'ecraser a ton premier atterrissage, compris ? Quoi qu'il en soit, tu vas devoir parler a 
-        Cornee a l'observatoire pour obtenir les codes de lancement si tu veux pouvoir decoller. 
-        Amene-les-moi une fois que tu auras fait tes adieux ou je ne sais quoi."), nl.
+        Cornee a l'observatoire pour obtenir les codes de lancement si tu veux pouvoir decoller."), nl.
 
 
 
@@ -210,10 +258,9 @@ dire(ok) :-
 decrire(reveil) :-
     write("Vous vous reveillez dans la nature, observant le ciel. 
     Vous voyez les etoiles et distinguer une forme exploser au loin... 
-    Une planete de tres grande taille parcourt tranquillement ton champ de vision.
-    "), nl.
+    Une planete de tres grande taille parcourt tranquillement ton champ de vision."), nl.
 
-decrire(feu_de_camp) :-
+decrire(camp) :-
     write("Le feu crepite et eclaire la nuit, un villageois se tient pres du bois crepitant.
     Une fusee se tient en hauteur, cela vous donne le tournis. Un ascenseur pres de vous permet d'y acceder.
     Au dela du feu, se tient un chemin menant vers un batiment eclairee...
@@ -221,11 +268,25 @@ decrire(feu_de_camp) :-
 
 
 decrire(musee) :-
-    write("Vous rentrez, ebloui, dans un grand batiment avec une multitude de statues, maquettes et d'etranges cailloux.
+    write("Vous rentrez, ebloui, dans un grand batiment avec une multitude de statues, maquettes et d'etranges sigles sur le mur.
     Chaque objet semble plus interessant l'un que l'autre.
-    -> (Options : voir statue, parler, voir maquettes)"), nl.
+    -> (Options : voir statue, voir maquettes, voir sigles, aller etage, aller camp)"), nl.
 
-decrire(musee_etage) :-
-    write(""), nl.
+decrire(etage) :-
+    write("Vous arrivez a l'etage du musee, une grande bais vitree au plafond permet de voir les etoiles.
+    Cornee, le gerant du musee, se tient pres de son bureau.
+    -> (Options : parler, aller musee)"), nl.
 
+decrire(fusee) :-
+        position_courante(camp),
+        write("Vous arrivez a l'ascenseur, entrez les codes de lancement et arrivez a la plateforme de lancement ou vous attend votre fusee.
+        Vous entrez par l'ecoutille, vous voici dans votre vaisseau spatial.
+        Vous pouvez y voir votre combinaison spatial, le journal de bord ainsi que les commandes du vaisseau.
+        -> (Options : prendre combinaison, voir journal, aller espace, aller camp)"), nl.
+
+decrire(espace) :-
+    write("Vous prenez les commandes de votre vaisseau, allumez les moteur et vous envolez.
+    Vous dépassez la ligne d'horizon à vive allure et ne voyez plus que les etoiles dans le noir...
+    Vous etes desormais dans l'espace, vous pouvez aller ou vous voulez !
+    -> (Options : aller cravite, aller leviathe, aller station, aller comete, aller atrebois)"), nl.
 
